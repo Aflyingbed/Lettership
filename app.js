beac5e09e6e0852070fd6111efc0f65284eae5d8
Sam <sam123dbz@gmail.com>
2024-11-07 05:08:25 +0500

    Fixed Cookies 2

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(
 		resave: false,
 		saveUninitialized: false,
 		cookie: {
-			secure: process.env.NODE_ENV === "production",
+			secure: true,
 			maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
 		},
 	}),
