948a3490f71028e9eb2933a4eea255379cbc5e0f
Sam <sam123dbz@gmail.com>
2024-11-07 06:55:11 +0500

    SAS

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(
 		cookie: {
 			secure: process.env.NODE_ENV === "production",
 			maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+			httpOnly: true,
+			sameSite: "none",
 		},
 	}),
 );
