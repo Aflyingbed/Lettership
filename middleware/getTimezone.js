2356bef5c401e9d8e43375a446aed0bce125fd0f
Sam <sam123dbz@gmail.com>
2024-11-08 15:04:59 +0500

    Fixed?

--- middleware/getTimezone.js
+++ middleware/getTimezone.js
@@ -3,14 +3,17 @@ const axios = require("axios");
 async function getTimezone(req, res, next) {
 	try {
 		const ip =
-			req.headers["x-forwarded-for"]?.split(",")[0] ||
+			req.ip ||
+			req.headers["x-forwarded-for"] ||
+			req.connection.remoteAddress ||
+			req.socket.remoteAddress ||
 			req.connection.remoteAddress;
-            
+
 		const response = await axios.get(
 			`https://ipinfo.io/${ip}?token=${process.env.IPINFO_API}`,
 		);
-		
-		req.userTimezone = response.data.timezone || "UTC";
+
+		req.userTimezone = response.data.timezone || "Asia/Shanghai";
 	} catch (error) {
 		console.error("Error fetching timezone:", error.message);
 		req.userTimezone = "UTC";
