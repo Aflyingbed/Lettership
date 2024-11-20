4ae2cbf62d77684cdd145133cbc53eaa69498f7c
Sam <sam123dbz@gmail.com>
2024-11-20 05:13:22 +0500

    Optimized sfx

--- public/scripts/font.js
+++ public/scripts/font.js
@@ -1,29 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fontOptions = document.querySelectorAll("[data-font]");
   const defaultFont = "font-['Insta']";
-
   const savedFont = localStorage.getItem("fontClass") || defaultFont;
-  document.body.className = savedFont;
 
+  const soundEffects = {
+    "font-['Special-Elite']": {
+      path: "../sfx/font/im-spongebob.mp3",
+      audio: null,
+    },
+    "font-['Papyrus']": { path: "../sfx/font/freaky-ahh.mp3", audio: null },
+    "font-['Macondo']": {
+      path: "../sfx/font/keep-your-secrets.mp3",
+      audio: null,
+    },
+    "font-['Tiny5']": {
+      path: "../sfx/font/evidence-jingle-2013.mp3",
+      audio: null,
+    },
+    "font-['Insta']": { path: "../sfx/font/up-meow.mp3", audio: null },
+    "font-['Edo']": { path: "../sfx/font/kiryu-chan.mp3", audio: null },
+    "font-['DS']": {
+      path: "../sfx/font/dark-souls-great-grey-wolf-sif.mp3",
+      audio: null,
+    },
+    "font-['Comic']": { path: "../sfx/font/pizza-time.mp3", audio: null },
+  };
+
+  Object.entries(soundEffects).forEach(([font, sound]) => {
+    sound.audio = new Audio(sound.path);
+    sound.audio.load();
+  });
+
+  document.body.className = savedFont;
   fontOptions.forEach((option) => {
     if (option.getAttribute("data-font") === savedFont) {
       option.checked = true;
     }
   });
 
-  const soundEffects = {
-    "font-['Special-Elite']": "../sfx/font/im-spongebob.mp3",
-    "font-['Papyrus']": "../sfx/font/freaky-ahh.mp3",
-    "font-['Macondo']": "../sfx/font/keep-your-secrets.mp3",
-    "font-['Tiny5']": "../sfx/font/evidence-jingle-2013.mp3",
-    "font-['Insta']": "../sfx/font/up-meow.mp3",
-    "font-['Edo']": "../sfx/font/kiryu-chan.mp3",
-    "font-['DS']": "../sfx/font/dark-souls-great-grey-wolf-sif.mp3",
-    "font-['Comic']": "../sfx/font/pizza-time.mp3",
-  };
-
   let currentAudio = null;
-
   fontOptions.forEach((option) => {
     option.addEventListener("change", () => {
       const newFont = option.getAttribute("data-font");
@@ -34,11 +49,11 @@ document.addEventListener("DOMContentLoaded", () => {
         currentAudio.pause();
         currentAudio.currentTime = 0;
       }
-
-      const sfxPath = soundEffects[newFont];
-      if (sfxPath) {
-        currentAudio = new Audio(sfxPath);
-        currentAudio.play();
+      if (soundEffects[newFont]) {
+        currentAudio = soundEffects[newFont].audio;
+        currentAudio
+          .play()
+          .catch((err) => console.log("Could not play sound:", err));
       }
     });
   });
--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const themeControllers = document.querySelectorAll(".theme-controller");
   const defaultTheme = "dark";
-
   const savedTheme = localStorage.getItem("theme") || defaultTheme;
+  
+  const themeSounds = {
+    halloween: { path: "../sfx/theme/oh-my-gah.mp3", audio: null },
+    dark: { path: "../sfx/theme/notification-bell.mp3", audio: null },
+    coffee: { path: "../sfx/theme/zelda-cooking-normal.mp3", audio: null },
+    synthwave: { path: "../sfx/theme/cyberpunk-2077.mp3", audio: null },
+    sunset: {
+      path: "../sfx/theme/i-just-wanna-be-part-of-your-symphony.mp3",
+      audio: null,
+    },
+    cupcake: { path: "../sfx/theme/mii-channel.mp3", audio: null },
+    pastel: { path: "../sfx/theme/chocobo_wark.mp3", audio: null },
+    valentine: { path: "../sfx/theme/fein-x-nokia.mp3", audio: null },
+  };
+
+  Object.entries(themeSounds).forEach(([theme, sound]) => {
+    sound.audio = new Audio(sound.path);
+    sound.audio.load();
+  });
+
   if (savedTheme) {
     document.documentElement.setAttribute("data-theme", savedTheme);
     const selectedInput = document.querySelector(
@@ -13,19 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  const themeSounds = {
-    halloween: "../sfx/theme/oh-my-gah.mp3",
-    dark: "../sfx/theme/notification-bell.mp3",
-    coffee: "../sfx/theme/zelda-cooking-normal.mp3",
-    synthwave: "../sfx/theme/cyberpunk-2077.mp3",
-    sunset: "../sfx/theme/i-just-wanna-be-part-of-your-symphony.mp3",
-    cupcake: "../sfx/theme/mii-channel.mp3",
-    pastel: "../sfx/theme/chocobo_wark.mp3",
-    valentine: "../sfx/theme/fein-x-nokia.mp3",
-  };
-
   let currentThemeAudio = null;
-
   themeControllers.forEach((controller) => {
     controller.addEventListener("change", (e) => {
       const selectedTheme = e.target.value;
@@ -36,11 +43,11 @@ document.addEventListener("DOMContentLoaded", () => {
         currentThemeAudio.pause();
         currentThemeAudio.currentTime = 0;
       }
-
-      const sfxPath = themeSounds[selectedTheme];
-      if (sfxPath) {
-        currentThemeAudio = new Audio(sfxPath);
-        currentThemeAudio.play();
+      if (themeSounds[selectedTheme]) {
+        currentThemeAudio = themeSounds[selectedTheme].audio;
+        currentThemeAudio
+          .play()
+          .catch((err) => console.log("Could not play sound:", err));
       }
     });
   });
